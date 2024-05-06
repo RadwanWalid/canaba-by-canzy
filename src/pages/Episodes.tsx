e7a3f58faa1be5c5e72dd20d58955384864886c5
RadwanWalid <132624884+Radwan-Walid@users.noreply.github.com>
2024-05-06 17:37:17 +0300

    Added New Epsiode

--- src/pages/Episodes.tsx
+++ src/pages/Episodes.tsx
@@ -2,15 +2,20 @@ import YoutubePlayer from "../components/YoutubePlayer/YoutubePlayer";
 
 const Episodes = () => {
 
-  const links: string[] = ['https://www.youtube.com/embed/RsMBeBiBph0?si=aLoqSGmZG2pr5YzX', 'https://www.youtube.com/embed/U0AbAfKEGXM?si=ug2ILeU9ojdh_RvA', 'https://www.youtube.com/embed/bgs0acrZXBg?si=NNO3c32meId4t8oy'];
+  const links: string[] = [
+    'https://www.youtube.com/embed/-uLTrI4tqmE?si=WGzH1Jq8ys89Eghg',
+    'https://www.youtube.com/embed/RsMBeBiBph0?si=aLoqSGmZG2pr5YzX', 
+    'https://www.youtube.com/embed/U0AbAfKEGXM?si=ug2ILeU9ojdh_RvA', 
+    'https://www.youtube.com/embed/bgs0acrZXBg?si=NNO3c32meId4t8oy',
+  ];
 
   return (
     <div className="relative space-y-14 px-16 blog:px-10">
       <div className="relative flex blog:flex-col blog:space-x-0 items-center justify-between space-x-16 h-full pt-12">
         <div className="flex-1 space-y-4 blog:text-center">
-          <h1 className="text-6xl blog:text-2xl italic font-medium">Women in Egypt- Fatma Sharmy</h1>
+          <h1 className="text-6xl blog:text-2xl italic font-medium">Women in Egypt- Menna Khalil</h1>
           <p className="text-[#B8B8B8]">Watch our latest episode.</p>
-          <a href="https://www.youtube.com/embed/bgs0acrZXBg?si=NNO3c32meId4t8oy" target="_blank" className="blog:hidden mt-4 rounded-md py-2.5 inline-block px-6 border-2 text-primary border-primary hover:text-[#F5F5F5] hover:bg-primary transition-all duration-200">Watch Now</a>
+          <a href="https://youtu.be/-uLTrI4tqmE?si=QW8J3EJiD30VFra-" target="_blank" className="blog:hidden mt-4 rounded-md py-2.5 inline-block px-6 border-2 text-primary border-primary hover:text-[#F5F5F5] hover:bg-primary transition-all duration-200">Watch Now</a>
         </div>
         <div className="relative drop-shadow-lg blog:mt-10">
           <div className="bg-primary w-full rounded-xl absolute -right-6 -top-4 bottom-4" />
